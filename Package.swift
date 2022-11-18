dd4302ab6f02b16a6adea3f74e381cd5d61bdd15
Andrey Samoshkin <crees007@rambler.ru>
2022-11-18 15:56:27 +0300

    Update Package lowest versions

--- Package.swift
+++ Package.swift
@@ -5,7 +5,7 @@ import PackageDescription
 
 let package = Package(
     name: "WebRTC",
-    platforms: [.iOS(.v10), .macOS(.v10_11)],
+    platforms: [.iOS(.v11), .macOS(.v10_13)],
     products: [
         .library(
             name: "WebRTC",
